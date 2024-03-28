29158e19799b8f167ce0119b3e85cdbcc4280772
irfanfrq <irfanfaruqzn@gmail@gmail.com>
2024-03-28 20:54:25 +0700

    Update Smbd

--- modul-1/220441100159-M.IrfanFaruqZain/Modul1.sql
+++ modul-1/220441100159-M.IrfanFaruqZain/Modul1.sql
@@ -1,18 +1,16 @@
- -- -- --  MEMBUAT DATABASE -- -- --
 CREATE SCHEMA swalayan;
 
--- -- --  MEMBUAT SET DEFAULT PADA DATABASE -- -- --
 USE swalayan;
 
-
--- -- --  MEMBUAT TABEL -- -- --
+#MEMBUAT TABEL 
 CREATE TABLE barang( 
     id_barang INT(20) NOT NULL PRIMARY KEY,
     nama_barang VARCHAR(50) NOT NULL,
     jmlh_stok INT(20) NOT NULL,
     hrg_jual INT (20) NOT NULL, 
     hrg_beli INT(20) NOT NULL,
-    id_supplier INT(20) NOT NULL);
+    id_supplier INT(20) NOT NULL,
+    FOREIGN KEY (id_supplier)REFERENCES supplier(id_supplier));
     
     
 CREATE TABLE supplier(
@@ -28,7 +26,9 @@ CREATE TABLE keuangan(
     tipe ENUM ("Penjualan", "Pembelian"),
     kategori ENUM ("Lunas", "Hutang"),
     tanggal_keuangan DATE NOT NULL,
-    harga_total INT(20) NOT NULL);
+    harga_total INT(20) NOT NULL,
+    FOREIGN KEY (id_barang) REFERENCES barang(id_barang),
+    FOREIGN KEY (id_stok)REFERENCES stok (id_stok));
     
 CREATE TABLE stok(
     id_stok INT(20) NOT NULL PRIMARY KEY, 
@@ -41,14 +41,16 @@ CREATE TABLE pembelian(
     id_barang INT(20) NOT NULL,
     jmlh_beli INT(20) NOT NULL,
     tgl_pembeli DATE NOT NULL,
-    kategori ENUM ("Lunas", "Hutang") NOT NULL);
+    kategori ENUM ("Lunas", "Hutang") NOT NULL,
+    FOREIGN KEY (id_barang)REFERENCES barang (id_barang));
     
 CREATE TABLE penjualan(
     id_penjual INT(20) NOT NULL PRIMARY KEY,
     id_barang INT(20) NOT NULL,
     jmlh_jual INT(20) NOT NULL,
     tgl_penjualan DATE NOT NULL,
-    total INT(20) NOT NULL);
+    total INT(20) NOT NULL,
+    FOREIGN KEY (id_barang) REFERENCES barang (id_barang));
     
 CREATE TABLE retur(
     id_retur INT(20) NOT NULL PRIMARY KEY,
@@ -56,26 +58,27 @@ CREATE TABLE retur(
     id_supplier INT(20) NOT NULL,
     jmlh_retur INT(20) NOT NULL,
     tgl_retur DATE NOT NULL,
-    alasan ENUM ("Kadaluarsa", "Rusak", "Dll") NOT NULL);
+    alasan ENUM ("Kadaluarsa", "Rusak", "Dll") NOT NULL,
+    FOREIGN KEY (id_barang) REFERENCES barang(id_barang),
+    FOREIGN KEY (id_supplier)REFERENCES supplier(id_supplier));
     
-CREATE TABLE laporan(
-    id_laporan INT(20) NOT NULL PRIMARY KEY,
-    id_barang INT(20) NOT NULL,
-    lprn_penjualan INT(20) NOT NULL,
-    lprn_stok INT(20) NOT NULL);
 
--- -- --  INSERT KE TABEL -- -- --
+SHOW TABLES;
+DROP TABLE barang;
+DESC barang;
+SELECT * FROM barang;
+#PENGISIAN TABEL
 INSERT INTO barang (`id_barang`, `nama_barang`, `jmlh_stok`, `hrg_jual`, `hrg_beli`, `id_supplier`) VALUES 
-    (1, 'Rokok', 12, 25000, 24000, 1122),
-    (2, 'Shampo', 20, 7000, 6500, 1122),
-    (3, 'Sabun', 10, 5000, 4000, 1212),
-    (4, 'Rinso', 12, 8000, 7000, 1212),
-    (5, 'Permen', 14, 1000, 500, 2121),
-    (6, 'Bygon', 7, 14000, 13000, 2121),
-    (7, 'Mie Instan', 20, 4000, 3000, 2211),
-    (8, 'Minyak', 10, 12000, 10000, 2211),
-    (9, 'Lampu', 17, 14000, 12500, 1232),
-    (10,'Teh Pucuk', 18, 4000, 3500, 1232);
+    (1, 'Rokok', 12, 25000, 24000, 2012),
+    (2, 'Shampo', 20, 7000, 6500, 2011),
+    (3, 'Sabun', 100, 5000, 4000, 2122),
+    (4, 'Rinso', 12, 8000, 7000, 2211),
+    (5, 'Permen', 14, 1000, 500, 2311),
+    (6, 'Bygon', 7, 14000, 13000, 2212),
+    (7, 'Mie Instan', 20, 4000, 3000, 1121),
+    (8, 'Minyak', 10, 12000, 10000, 1021),
+    (9, 'Lampu', 17, 14000, 12500, 1002),
+    (10,'Teh Pucuk', 18, 4000, 3500, 1122);
     
 INSERT INTO supplier VALUES
     (2012, "Junaidi", "Bangkalan", "Lunas"),
@@ -88,18 +91,21 @@ INSERT INTO supplier VALUES
     (1021, "Mulyani", "Gresik", "Lunas"),
     (1002, "Paijo", "Lamongan", "Hutang"),
     (1122, "Purnomo", "Surabaya", "Lunas");
+SELECT * FROM keuangan;
 
 INSERT INTO keuangan VALUES 
-    (3399, 1, 1002, "Penjualan", "Hutang", '2023-03-1', 400000),
-    (4433, 2, 1003, "Pembelian", "Lunas", '2023-03-02', 270000),
-    (4455, 3, 1004, "Pembelian", "Hutang", '2023-03-12', 120000),
-    (5544, 4, 1005, "Penjualan", "Hutang", '2023-03-11', 300000),
-    (2211, 5, 1006, "Penjualan", "Lunas", '2023-03-22', 250000),
-    (2233, 6, 1007, "Pembelian", "Lunas", '2023-03-15', 180000),
-    (1122, 7, 1008, "Penjualan", "Hutang", '2023-03-16', 320000),
-    (5533, 8, 1009, "Pembelian", "Hutang", '2023-03-18', 210000),
-    (4411, 9, 2002, "Pembelian", "Lunas", '2023-03-21', 270000),
-    (1155,10, 2003, "Pembelian", "Lunas", '2023-03-24', 150000);
+    (3399, 1, 10012, "Penjualan", "Hutang", '2023-03-1', 400000),
+    (4433, 2, 10011, "Pembelian", "Lunas", '2023-03-02', 270000),
+    (4455, 3, 10022, "Pembelian", "Hutang", '2023-03-12', 120000),
+    (5544, 4, 10033, "Penjualan", "Hutang", '2023-03-11', 300000),
+    (2211, 5, 10044, "Penjualan", "Lunas", '2023-03-22', 250000),
+    (2233, 6, 10055, "Pembelian", "Lunas", '2023-03-15', 180000),
+    (1122, 7, 10066, "Penjualan", "Hutang", '2023-03-16', 320000),
+    (5533, 8, 10077, "Pembelian", "Hutang", '2023-03-18', 210000),
+    (4411, 9, 10088, "Pembelian", "Lunas", '2023-03-21', 270000),
+    (1155,10, 10099, "Pembelian", "Lunas", '2023-03-24', 150000);
+    
+SELECT * FROM keuangan;
 
 INSERT INTO stok VALUES
     (10012, "Rokok", 50, 15),
@@ -112,6 +118,8 @@ INSERT INTO stok VALUES
     (10077, "Minyak", 50, 20),
     (10088, "Lampu", 50, 20),
     (10099, "Teh pucuk", 50, 10);
+ 
+SELECT * FROM stok;
 
 INSERT INTO pembelian VALUES
     (2001, 1, 50 ,'2023-02-22', "Lunas"),
@@ -138,16 +146,16 @@ INSERT INTO penjualan VALUES
     (410, 10,4, '2023-03-30', 52000);
 
 INSERT INTO retur VALUES 
-    (501, 1, 1101, 6, '2023-01-11', "Rusak"),
-    (502, 2, 1102, 3, '2023-01-12', "Rusak"),
-    (503, 3, 1103, 2, '2023-01-11', "Kadaluarsa"),
-    (504, 4, 1104, 1, '2023-01-12', "Rusak"),
-    (505, 5, 1105, 3, '2023-01-08', "Kadaluarsa"),
-    (506, 6, 1106, 4, '2023-01-03', "Kadaluarsa"),
-    (507, 7, 1107, 2, '2023-01-08', "Rusak"),
-    (508, 8, 1108, 3, '2023-01-09', "Rusak"),
-    (509, 9, 1109, 5, '2023-01-10', "Kadaluarsa"),
-    (510,10, 1110, 3, '2023-01-12', "Rusak");
+    (501, 1, 2012, 6, '2023-01-11', "Rusak"),
+    (502, 2, 2011, 3, '2023-01-12', "Rusak"),
+    (503, 3, 2122, 2, '2023-01-11', "Kadaluarsa"),
+    (504, 4, 2211, 1, '2023-01-12', "Rusak"),
+    (505, 5, 2311, 3, '2023-01-08', "Kadaluarsa"),
+    (506, 6, 2212, 4, '2023-01-03', "Kadaluarsa"),
+    (507, 7, 1121, 2, '2023-01-08', "Rusak"),
+    (508, 8, 1021, 3, '2023-01-09', "Rusak"),
+    (509, 9, 1002, 5, '2023-01-10', "Kadaluarsa"),
+    (510,10, 1122, 3, '2023-01-12', "Rusak");
 
 
 -- -- --  MENAMPILKAN TABEL -- -- --
